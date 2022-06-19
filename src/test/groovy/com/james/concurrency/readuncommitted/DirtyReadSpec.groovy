9bd966feee9029f82ec399b3eb4da65a4893d6a7
james <chenghan_yang@163.com>
2022-06-19 11:21:02 +0800

    提升隔离级别至 read committed或以上解决脏读

--- src/test/groovy/com/james/concurrency/readuncommitted/DirtyReadSpec.groovy
+++ src/test/groovy/com/james/concurrency/readuncommitted/DirtyReadSpec.groovy
@@ -25,12 +25,13 @@ class DirtyReadSpec extends Specification {
     def "read uncommitted 隔离级别下产生脏读"() {
         given:
         bankAccountMapper.updateBalanceById(0, 1L);
-        // 1. 新建程写入脏数据
+        // 线程(事务)A
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         executorService.execute(() -> {
             transactionTemplate.execute(new TransactionCallbackWithoutResult() {
                 protected void doInTransactionWithoutResult(TransactionStatus status) {
                     BankAccount bankAccount = bankAccountMapper.selectById(1L);
+                    // 1. 线程A写入脏数据
                     bankAccountMapper.updateBalanceById(bankAccount.getBalance() + 1, 1L);
                     // 3. 制造脏数据被使用的间隙
                     try {
@@ -38,11 +39,13 @@ class DirtyReadSpec extends Specification {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    // 6. 线程A回滚
                     status.setRollbackOnly();
                 }
             });
         });
 
+        // 线程(事务)B
         executorService.execute(() -> {
             transactionTemplate.execute(new TransactionCallbackWithoutResult() {
                 protected void doInTransactionWithoutResult(TransactionStatus status) {
@@ -65,5 +68,54 @@ class DirtyReadSpec extends Specification {
         expect:
         // 虽然回滚了，但是发生脏读，总数为2
         bankAccountMapper.selectById(1L).getBalance() == 2
+    }
+
+
+    def "提升隔离级别至 read committed或以上解决脏读"() {
+        given:
+        bankAccountMapper.updateBalanceById(0, 1L);
+        // 线程(事务)A
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.execute(() -> {
+            transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+                protected void doInTransactionWithoutResult(TransactionStatus status) {
+                    BankAccount bankAccount = bankAccountMapper.selectById(1L);
+                    // 1. 线程A写入脏数据
+                    bankAccountMapper.updateBalanceById(bankAccount.getBalance() + 1, 1L);
+                    // 3. 制造脏数据被使用的间隙
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    // 6. 线程A回滚
+                    status.setRollbackOnly();
+                }
+            });
+        });
+
+        // 线程(事务)B
+        executorService.execute(() -> {
+            transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+                protected void doInTransactionWithoutResult(TransactionStatus status) {
+                    // 2. 等待脏数据写入
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    // 4. 脏数据被读取
+                    BankAccount bankAccount = bankAccountMapper.selectById(1L);
+                    // 5. 脏数据被写入
+                    bankAccountMapper.updateBalanceById(bankAccount.getBalance() + 1, 1L);
+                }
+            });
+        });
+        executorService.shutdown();
+        while(! executorService.isTerminated());
+
+        expect:
+        // rc及以上隔离级别避免脏读
+        bankAccountMapper.selectById(1L).getBalance() == 1
     }
 }
