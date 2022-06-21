caac10889c5abe608ff9dc602955d8375d121022
james <chenghan_yang@163.com>
2022-06-21 22:27:37 +0800

    验证RR级别下更新会加gap锁从而解决幻读问题

--- src/main/java/com/james/concurrency/mapper/BankAccountMapper.java
+++ src/main/java/com/james/concurrency/mapper/BankAccountMapper.java
@@ -13,17 +13,29 @@ public interface BankAccountMapper {
 
     BankAccount selectByIdForUpdate(@Param("id") Long id);
 
+    List<BankAccount> selectListByBalanceGt(@Param("balance") int balance);
+
+    List<BankAccount> selectListByName(@Param("name") String name);
+
+    List<BankAccount> selectListByBalanceGtForUpdate(@Param("balance") int balance);
+
+    List<BankAccount> selectListByBalance(@Param("balance") int balance);
+
+    BankAccount selectByIdLockInShareMode(@Param("id")Long id);
+
+
     void updateBalanceById(@Param("balance") int balance, @Param("id")Long id);
 
     void atomicUpdateBalanceByCostAndId(@Param("cost") int cost, @Param("id")Long id);
 
-    BankAccount selectByIdLockInShareMode(@Param("id")Long id);
+    void updateBalanceByBalanceGt(@Param("balance") int balance, @Param("currentBalance") int currentBalance);
 
-    List<BankAccount> selectListByBalanceGt(@Param("balance") int balance);
+    void updateNameByBalanceGt(@Param("name") String name, @Param("balance") int balance);
 
-    List<BankAccount> selectListByBalanceGtForUpdate(@Param("balance") int balance);
+    void deleteByName(@Param("name")String name);
+
+    void updateById(BankAccount bankAccount);
 
     long insert(BankAccount bankAccount);
 
-    void deleteByName(@Param("name")String name);
 }
